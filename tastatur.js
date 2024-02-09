9d0dd86941710a9f85ba4cca24cd8859d411f584
Namhaiqc <157683555+Namhaiqc@users.noreply.github.com>
2024-02-09 14:53:26 +0100

    Add files via upload

---
+++ tastatur.js
@@ -0,0 +1,11 @@
+let hai = document.getElementById('hai')
+
+let letterPressed = htmlElement.innerHTML;
+
+function pushedLetter(htmlElement) { 
+    let letterPressed = htmlElement.innerHTML;
+
+    console.log(letterPressed);
+hai.innerHTML+= letterPressed;
+}
+
